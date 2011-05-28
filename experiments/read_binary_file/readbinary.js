f8e333d1c126811c1595ba0ad1ef07a4e7d9e290
Amos Wenger <amos@official.fm>
2011-05-28 11:02:55 +0200

    read as binary experiment is a success!

--- experiments/read_binary_file/readbinary.js
+++ experiments/read_binary_file/readbinary.js
@@ -1,12 +1,25 @@
 
+console.log("readbinary.js loaded");
+
 function readFile() {
+    console.log("readFile called");
+
     // uploadData is a form element
     // fileChooser is input element of type 'file'
     var file = document.forms['uploadData']['fileChooser'].files[0];
     
-    if(file)
-    {
-      // Perform file ops
-      console.log("Got file = " + file.name);
-    }
+    if(!file) return;
+    
+    // Perform file ops
+    console.log("Got file = " + file.name);
+    var reader = new FileReader();
+    
+    reader.onloadend = function (evt) {
+      var data = evt.target.result;
+      console.log("Reading a " + (data.length / 1024) + "KB file");
+      console.log("Data = " + data[0] + "" + data[1] + "" + data[2]);
+    };
+    
+    reader.readAsBinaryString(file);
+    return false;
 }
