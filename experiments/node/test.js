b66c7d1671f359d54082b1d7c025b3a4ce8cd4ed
matti <matti@orgor.(none)>
2011-05-31 03:02:25 +0200

    fixed pow and implemented subarray methods for typed arrays

--- experiments/node/test.js
+++ experiments/node/test.js
@@ -6,6 +6,28 @@ ArrayBuffer     = array.ArrayBuffer;
 Int32Array      = array.Int32Array;
 Float32Array    = array.Float32Array;
 
+
+Float32Array.prototype.subarray = function(start) {
+    var copy = new Float32Array(new ArrayBuffer(this.length - 4 * start));
+    
+    for (var i = 0; i < copy.length; i++) {
+        copy[i] = this[i + start];
+    }
+
+    return copy
+};
+
+Int32Array.prototype.subarray = function(start) {
+    var copy = new Int32Array(new ArrayBuffer(this.length - 4 * start));
+    
+    for (var i = 0; i < copy.length; i++) {
+        copy[i] = this[i + start];
+    }
+
+    return copy;
+};
+
+
 require('../../mad.js');
 require('../../id3.js');
 require('../../layer3.js');
