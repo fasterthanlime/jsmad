ee89710a71091b140d27041a91f674317cf841bc
Amos Wenger <amos@official.fm>
2011-06-01 15:49:13 +0200

    Fixed typo in layer3 (sw/reorder_sw), added two mp3 files, huffman test, beginning of output test

---
+++ experiments/node/huffdecode.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var sys = require('sys');
+
+require('./typed-array.js');
+require('../../mad.js');
+require('../../id3.js');
+require('../../layer3.js');
+require('../../synth.js');
+require("../../mad.js");
+require("../../rq_table.js");
+require("../../imdct_s.js");
+require("../../huffman.js");
+require("../../bit.js");
+require("../../stream.js");
+require("../../id3.js");
+require("../../layer3.js");
+require("../../frame.js");
+require("../../synth.js");
+
+var data = fs.readFileSync("one_second_of_silence.mp3", "binary");
+// var data = fs.readFileSync("soul-2.mp3", "binary");
+
+console.log("Reading a " + Math.round(data.length / 1024) + "KB file");
+
+var stream = new Mad.Stream(data);
+
+ID3_skipHeader(stream);
+
+var STEPS_COUNT = 0;
+
+var frame = null;
+
+frame = Mad.Frame.decode(stream); // apparently first frame has nothing to decode
+frame = Mad.Frame.decode(stream);
+
+console.log("error code: " + stream.error);
---
+++ experiments/node/output.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var sys = require('sys');
+
+require('./typed-array.js');
+require('../../mad.js');
+require('../../id3.js');
+require('../../layer3.js');
+require('../../synth.js');
+require("../../mad.js");
+require("../../rq_table.js");
+require("../../imdct_s.js");
+require("../../huffman.js");
+require("../../bit.js");
+require("../../stream.js");
+require("../../id3.js");
+require("../../layer3.js");
+require("../../frame.js");
+require("../../synth.js");
+
+// var data = fs.readFileSync("one_second_of_silence.mp3", "binary");
+// var data = fs.readFileSync("one_second_beep.mp3", "binary");
+var data = fs.readFileSync("output.mp3", "binary");
+// var data = fs.readFileSync("soul-2.mp3", "binary");
+
+console.log("Reading a " + Math.round(data.length / 1024) + "KB file");
+
+var stream = new Mad.Stream(data);
+
+ID3_skipHeader(stream);
+
+var STEPS_COUNT = 0;
+
+var frame = null;
+
+while (frame = Mad.Frame.decode(stream)) {
+    var synth = new Mad.Synth();
+    synth.frame(frame);
+
+    var samples = synth.pcm.samples[0];
+
+    var min = 0.0;
+    var max = 0.0;
+    var mean = 0.0;
+    
+    for (var i in samples) {
+        var sample = samples[i];
+        mean = (mean + sample) / 2.0;
+        if(min > sample) min = sample;
+        if(max < sample) max = sample;
+    }
+
+    console.log("min = " + min + ", max = " + max + ", mean = " + mean);
+}
+
+console.log("error code: " + stream.error);
