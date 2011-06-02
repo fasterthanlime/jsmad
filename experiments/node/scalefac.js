cad4262b832139b09aa8136bd68d0bc858702400
Matthias Georgi <matti.georgi@gmail.com>
2011-06-02 21:29:51 +0200

    fixed scalefac testcode

--- experiments/node/scalefac.js
+++ experiments/node/scalefac.js
@@ -729,7 +729,7 @@ scalefac: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,0,3,1,0,4,3,0,2,0,0,0,0,0,0,0,0,0
 
 
 function ary2str(ary) {
-    return String.fromCharCode.apply(ary);
+    return String.fromCharCode.apply(this, ary);
 }
 
 
