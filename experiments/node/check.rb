7613079675a8588db4173f05035168f656b5cc8f
Matthias Georgi <matti.georgi@gmail.com>
2011-06-05 01:38:58 +0200

    optional debugging

--- experiments/node/check.rb
+++ experiments/node/check.rb
@@ -1,17 +1,18 @@
-Dir.chdir("../../../libmad") do
-  system("make minimad && ./minimad < one_second_beep.mp3 > ../jsmad/experiments/node/libmad.txt")
-end
+# Dir.chdir("../../../libmad") do
+#   system("make minimad && ./minimad < one_second_beep.mp3 > ../jsmad/experiments/node/libmad.txt")
+# end
 
-system("node test.js > jsmad.txt")
+# system("node test.js > jsmad.txt")
 
-libmad = File.readlines("libmad.txt")
-jsmad = File.readlines("jsmad.txt")
+libmad = File.readlines(ARGV[0])
+jsmad = File.readlines(ARGV[1])
+
+diffs = []
 
 libmad.zip(jsmad) do |a, b|
   a.split("\t").zip(b.split("\t")) do |x, y|
-    diff = (x.to_f - y.to_f).abs
-    if diff > 1.0e-5
-      puts diff
-    end
+    diffs << (x.to_f - y.to_f).abs
   end
 end
+
+puts diffs.max
--- experiments/node/test.js
+++ experiments/node/test.js
@@ -27,12 +27,17 @@ var data = fs.readFileSync("one_second_beep.mp3", "binary");
 var stream = new Mad.Stream(data);
 var synth = new Mad.Synth();
 
-Debug = {
-    iteration: 0,
-    huffdecode: fs.createWriteStream('huffdecode-js.txt'),
-    sample: fs.createWriteStream('sample-js.txt'),
-    pcm: fs.createWriteStream('pcm-js.txt')
-};
+if (process.argv[2] == '-d') {
+    Debug = {
+        iteration: 0,
+        huffdecode: fs.createWriteStream('huffdecode-js.txt'),
+        sample: fs.createWriteStream('sample-js.txt'),
+        pcm: fs.createWriteStream('pcm-js.txt')
+    };
+}
+else {
+    Debug = false;
+}
 
 ID3_skipHeader(stream);
 
@@ -65,11 +70,15 @@ for (var i = 0; i < len; i++) {
     var sample = pcm[i] * max;
     buf[(i * 2) + 0] = (sample & 0xff00) >> 8;
     buf[(i * 2) + 1] = sample & 0xff;
-    Debug.pcm.write(pcm[i].toFixed(8) + "\n");
+    if (Debug) {
+        Debug.pcm.write(pcm[i].toFixed(8) + "\n");        
+    }
 }
 
 fs.writeFileSync("out.raw", buf, 'binary');
 
-Debug.huffdecode.end();
-Debug.sample.end();
-Debug.pcm.end();
+if (Debug) {
+    Debug.huffdecode.end();
+    Debug.sample.end();
+    Debug.pcm.end();    
+}
