9469587ecece71084818a604a4128a1faeb1bef4
Amos Wenger <amos@official.fm>
2011-06-17 01:28:09 +0200

    Now works also for official.fm tracks

--- experiments/mhd/mhd.js
+++ experiments/mhd/mhd.js
@@ -14,66 +14,79 @@ function onProgress(current, total) {
 
 var globalPlayer = null;
 
+function domReady() {
+	document.getElementById('ofm').onkeypress = function (ev) {
+		if(ev.keyCode == 13) { // enter pressed?
+			var track_id = this.value;
+			var url = "http://sandbox.official.fm/jsmad-proxy/?id=" + track_id;
+			console.log("should play " + url);
+			Mad.Player.fromURL(url, usePlayer);
+			this.value = "";
+			return false;
+		}
+	};
+
+}
+
+function usePlayer (player) {
+	if(globalPlayer) globalPlayer.destroy();
+	globalPlayer = player;
+	if (player.id3) {
+		var id3 = player.id3.toHash();
+		var id3element = document.getElementById('ID3');
+
+		var id3string = "<div class='player'><div class='picture'>";
+		var pictures = id3['Attached picture'];
+
+		if (pictures) {
+			var mime = pictures[0].mime;
+			var enc = btoa(pictures[0].value);
+			id3string += "<img class='picture' src='data:" + mime + ';base64,' + enc + "' />";
+		} else {
+		  id3string += "<img class='picture' src='../images/nopicture.png' />";
+		}
+
+		id3string += "<a href='#' id='playpause' class='button play'></a>";
+		id3string += "<div class='timeline'><div id='progressbar'></div></div>";
+
+		id3string += "</div></div>";
+		id3string += "<div class='info'>";
+		id3string += "<h2>" + id3['Title/Songname/Content description'] + "</h2>";
+		id3string += "<h3>" + id3['Lead artist/Lead performer/Soloist/Performing group'] + "</h3>";
+		id3string += "<div class='meta'>";
+		id3string += "<p><strong>Album:</strong> " + id3['Album/Movie/Show title'] + "</p>";
+		id3string += "<p><strong>Track:</strong> " + id3['Track number/Position in set'] + "</p>";
+		id3string += "<p><strong>Year:</strong> " + id3['Year'] + "</p>";
+		id3string += "</div>";
+		id3string += "</div>";
+
+		id3element.innerHTML = id3string;
+	}
+
+	player.onProgress = onProgress;
+	document.getElementById('playpause').onclick = function () {
+		player.setPlaying(!player.playing);
+		return false;
+	};
+	
+	player.onPlay = function() {
+		document.getElementById('playpause').className = 'button pause';
+	};
+
+	player.onPause = function() {
+		document.getElementById('playpause').className = 'button play';
+	};
+
+	player.createDevice();
+}
+
 function readFile() {
     // uploadData is a form element
     // fileChooser is input element of type 'file'
     var file = document.forms['uploadData']['fileChooser'].files[0];
-
     if (!file) {
         return;
     }
 
-    Mad.Player.fromFile(file, function (player) {
-		if(globalPlayer) globalPlayer.destroy();
-        globalPlayer = player;
-        if (player.id3) {
-            var id3 = player.id3.toHash();
-            var id3element = document.getElementById('ID3');
-
-            var id3string = "<div class='player'><div class='picture'>";
-            var pictures = id3['Attached picture'];
-
-            if (pictures) {
-                var mime = pictures[0].mime;
-                var enc = btoa(pictures[0].value);
-                id3string += "<img class='picture' src='data:" + mime + ';base64,' + enc + "' />";
-            } else {
-              id3string += "<img class='picture' src='../images/nopicture.png' />";
-			}
-
-            id3string += "<a href='#' id='playpause' class='button play'></a>";
-            id3string += "<div class='timeline'><div id='progressbar'></div></div>";
-
-            id3string += "</div></div>";
-            id3string += "<div class='info'>";
-            id3string += "<h2>" + id3['Title/Songname/Content description'] + "</h2>";
-            id3string += "<h3>" + id3['Lead artist/Lead performer/Soloist/Performing group'] + "</h3>";
-            id3string += "<div class='meta'>";
-            id3string += "<p><strong>Album:</strong> " + id3['Album/Movie/Show title'] + "</p>";
-            id3string += "<p><strong>Track:</strong> " + id3['Track number/Position in set'] + "</p>";
-            id3string += "<p><strong>Year:</strong> " + id3['Year'] + "</p>";
-            id3string += "</div>";
-            id3string += "</div>";
-
-            id3element.innerHTML = id3string;
-        }
-
-        player.onProgress = onProgress;
-        document.getElementById('playpause').onclick = function () {
-			player.setPlaying(!player.playing);
-			return false;
-        };
-        
-        player.onPlay = function() {
-			document.getElementById('playpause').className = 'button pause';
-		};
-
-		player.onPause = function() {
-			document.getElementById('playpause').className = 'button play';
-		};
-
-        player.createDevice();
-    });
-
-    return;
+    Mad.Player.fromFile(file, usePlayer);
 }
