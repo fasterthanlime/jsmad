4fc3fbed0ec527e19e7ec97de1dafc4718c58a50
gliese1337 <chronosurfer@gmail.com>
2011-06-20 20:36:34 -0700

    Oops! Overwrote FileStream when I meant to push AjaxStream. Fixed.

--- src/filestream.js
+++ src/filestream.js
@@ -1,115 +1,35 @@
-Mad.AjaxStream = function(url) {
+Mad.FileStream = function(file, callback) {
     this.state = { 'offset': 0 };
     
-    var request = window.XMLHttpRequest ? new XMLHttpRequest() :  ActiveXObject("Microsoft.XMLHTTP");
+    var self = this, reader = new FileReader();
     
-    // pseudo-binary XHR
-    request.overrideMimeType('text/plain; charset=x-user-defined');
-    request.open('GET', url);
-    
-    this.state.request = request;
-    this.state.amountRead = 0;
-    this.state.inProgress = true;
-    this.state.callbacks = [];
-    
-    var self = this;
-    
-    var iteration = 0;
-    
-    var onstatechange = function () {
-        iteration += 1;
-        if ((self.state.callbacks.length > 0 && iteration % 64 == 0) || iteration % 256 == 0) {
-            self.updateBuffer();
-            
-            var newCallbacks = [];
-            
-            for (var i = 0; i < self.state.callbacks.length; i++) {
-                var callback = self.state.callbacks[i];
-                
-                if (callback[0] < self.state.amountRead) {
-					try {
-						callback[1]();
-					} catch (e) {
-						console.log(e);
-					}
-                } else {
-                    newCallbacks.push(callback);
-                }
-            }
-            
-            self.state.callbacks = newCallbacks;
-        }
-        
-        if (request.readyState == 4) {
-			self.state.amountRead = self.state.contentLength;
-            for (var i = 0; i < self.state.callbacks.length; i++) {
-                var callback = self.state.callbacks[i];
-                callback[1]();
-            }
-            
-            window.clearInterval(self.state.timer);
-            
-            self.state.inProgress = false;
-        }
+    reader.onload = function () {
+		self.length =
+		self.state.amountRead = 
+		self.state.contentLength = reader.result.length;
+		self.state.buffer = reader.result;
+		  
+		callback(self);
     }
     
-    request.onreadystatechange = onstatechange;
-    
-    this.state.timer = window.setInterval(onstatechange, 250);
-    
-    request.send(null);
-};
-
-Mad.AjaxStream.prototype = new Mad.ByteStream();
-
-Mad.AjaxStream.prototype.updateBuffer = function() {
-    if (!this.state.finalAmount) {
-        this.state.arrayBuffer = this.state.request.mozResponseArrayBuffer;
-        if(this.state.arrayBuffer) {
-			this.state.byteBuffer = new Uint8Array(this.state.arrayBuffer);
-			this.state.amountRead = this.state.arrayBuffer.byteLength;
-		} else {
-			this.state.buffer = this.state.request.responseText
-			this.state.amountRead = this.state.buffer.length;
-		}
-        
-		this.state.contentLength = this.state.request.getResponseHeader('Content-Length');
-		if(!this.state.contentLength) {
-			// if the server doesn't send a Content-Length Header, just use amountRead instead
-			// it's less precise at first, but once everything is buffered it becomes accurate.
-			this.state.contentLength = this.state.amountRead;
-		}
-    
-        if (!this.state.inProgress) {
-            this.state.finalAmount = true;
-        }
-        
-        return true;
-    } else {
-        return false;
+    reader.onerror = function () {
+        console.log("Error!");
     }
+    
+    reader.readAsBinaryString(file);
 };
 
-Mad.AjaxStream.prototype.absoluteAvailable = function(n, updated) {
-    if (n > this.state.amountRead) {
-        if (updated) {
-			throw new Error("buffer underflow with absoluteAvailable!");
-            return false;
-        } else if (this.updateBuffer()) {
-            return this.absoluteAvailable(n, true);
-        } else {
-            return false;
-        }
-    } else {
-        return true;
-    }
+Mad.FileStream.prototype = new Mad.ByteStream();
+
+Mad.FileStream.prototype.absoluteAvailable = function(n, updated) {
+    return n < this.state.amountRead;
 };
 
-Mad.AjaxStream.prototype.seek = function(n) {
+Mad.FileStream.prototype.seek = function(n) {
     this.state.offset += n;
 };
 
-Mad.AjaxStream.prototype.read = function(n) {
+Mad.FileStream.prototype.read = function(n) {
     var result = this.peek(n);
     
     this.seek(n);
@@ -117,66 +37,30 @@ Mad.AjaxStream.prototype.read = function(n) {
     return result;
 };
 
-Mad.AjaxStream.prototype.peek = function(n) {
+Mad.FileStream.prototype.peek = function(n) {
     try {
         return this.get(this.state.offset, n);
     } catch (e) {
-		throw new Error("buffer underflow with peek!");
+        throw 'TODO: THROW PEEK ERROR!';
     }
 };
 
-Mad.AjaxStream.prototype.get = function(offset, length) {
+Mad.FileStream.prototype.get = function(offset, length) {
     if (this.absoluteAvailable(offset + length)) {
-		var tmpbuffer = "";
-		if(this.state.byteBuffer) {
-			for(var i = offset; i < offset + length; i += 1) {
-				tmpbuffer = tmpbuffer + String.fromCharCode(this.state.byteBuffer[i]);
-			}
-		} else {
-			for(var i = offset; i < offset + length; i += 1) {
-				tmpbuffer = tmpbuffer + String.fromCharCode(this.state.buffer.charCodeAt(i) & 0xff);
-			}
-		}
-		return tmpbuffer;
+        return this.state.buffer.slice(offset, offset + length);
     } else {
-		throw new Error("buffer underflow with get!");        
-        return;
+        throw 'TODO: THROW GET ERROR!';
     }
 };
 
-Mad.AjaxStream.prototype.getb = function(offset, length) {
+Mad.FileStream.prototype.getb = function(offset, length) {
     if (this.absoluteAvailable(offset + length)) {
-		if(this.state.byteBuffer) {
-			return this.state.byteBuffer.subarray(offset, offset + length);
-		} else {
-			var buffer = new Uint8Array(length);
-			for(var j = 0, i = offset; i < offset + length; i += 1, j += 1) {
-				buffer[j] = this.state.buffer.charCodeAt(i) & 0xff;
-			}
-			return buffer;
+		var i,buffer = new Uint8Array(length);
+		for(i = 0; i < length; i++) {
+			buffer[i] = this.state.buffer.charCodeAt(i+offset);
 		}
+		return buffer;
     } else {
-		throw new Error("buffer underflow with get!");        
-        return;
+        throw 'TODO: THROW GET ERROR!';
     }
-};
-
-Mad.AjaxStream.prototype.getU8 = function(offset, bigEndian) {
-	if(this.state.byteBuffer) {
-		return this.state.byteBuffer[offset];
-	}
-		
-    return this.get(offset, 1).charCodeAt(0);
-};
-
-Mad.AjaxStream.prototype.requestAbsolute = function(n, callback) {
-    if (n < this.state.amountRead) {
-        callback();
-    } else {
-        this.state.callbacks.push([n, callback]);
-    }
-};
-
-Mad.AjaxStream.prototype.request = function(n, callback) {
-    this.requestAbsolute(this.state.offset + n, callback);
 };
