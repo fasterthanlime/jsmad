1ddf52c73ca0189b1ab78930ef03def152fe8760
gliese1337 <chronosurfer@gmail.com>
2011-06-21 14:22:05 -0700

    Added local implementation of .getb() for performance gains.

--- src/stringstream.js
+++ src/stringstream.js
@@ -1,16 +1,16 @@
 Mad.StringStream = function(string) {
     this.state = { offset: 0, buffer: string, amountRead: string.length, length: string.length };
-}
+};
 
 Mad.StringStream.prototype = new Mad.ByteStream();
 
 Mad.StringStream.prototype.absoluteAvailable = function(n, updated) {
-    return n < this.state['amountRead'];
-}
+    return n < this.state.amountRead;
+};
 
 Mad.StringStream.prototype.seek = function(n) {
-    this.state['offset'] += n;
-}
+    this.state.offset += n;
+};
 
 Mad.StringStream.prototype.read = function(n) {
     var result = this.peek(n);
@@ -18,24 +18,34 @@ Mad.StringStream.prototype.read = function(n) {
     this.seek(n);
     
     return result;
-}
+};
 
 Mad.StringStream.prototype.peek = function(n) {
-    if (this.available(n)) {
-        var offset = this.state['offset'];
-        
-        var result = this.get(offset, n);
-        
-        return result;
-    } else {
+    try {
+        return this.get(this.state.offset, n);
+    } catch (e) {
         throw 'TODO: THROW PEEK ERROR!';
     }
-}
+};
 
 Mad.StringStream.prototype.get = function(offset, length) {
     if (this.absoluteAvailable(offset + length)) {
-        return this.state['buffer'].slice(offset, offset + length);
+        return this.state.buffer.substr(offset, length);
     } else {
         throw 'TODO: THROW GET ERROR!';
     }
-}
+};
+
+Mad.StringStream.prototype.getb = function(offset, length) {
+    var i, j, buffer, str;
+    if (this.absoluteAvailable(offset + length)) {
+        buffer = new Uint8Array(length);
+	    str = this.state.buffer;
+        for(i = 0, j = offset; i < length; i++, j++){
+		    buffer[i] = str.charCodeAt(j);
+	    }
+	    return buffer;
+    } else {
+        throw 'TODO: THROW GET ERROR!';
+    }
+};
